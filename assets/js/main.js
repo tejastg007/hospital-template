20bfdcbcfacb3e23451717d4ac33522430637d94
tejas <tejastg007@gmail.com>
2021-10-30 01:44:00 +0530

    implemented navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,18 @@ var swiper = new Swiper(".mySwiper", {
     // loop: true,
 });
 new WOW().init();
+function navAction(e) {
+    document.getElementById("nav").classList.toggle("nav-opened");
+}
+document.getElementById("navAction").addEventListener("click", (event) => {
+    event.preventDefault();
+    navAction(event);
+});
+document.getElementById("nav").addEventListener("click", (event) => {
+    tag = event.target.tagName.toLowerCase();
+    if (tag == "li") {
+        navAction();
+    } else {
+
+    }
+});
